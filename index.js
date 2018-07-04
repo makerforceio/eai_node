81745a9e792b6b6ee69a23b1806e33fb45cdf840
Isaac Tay <IsaacTay@programmer.net>
2018-07-04 19:53:16 +0800

    Works now

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var express = require('express')
 var app = express()
 
+
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-
-app.set('port', process.env.PORT || 3000);
 
 var min = 3;
 var pad = 2;
@@ -19,44 +18,69 @@ client.on("error", function (err) {
 });
 
 app.get('/init', function (req, res) {
-  var max_list = 0;
-  var max_length = 0;
+  multi = client.multi();
   for (var i = 0; i < min+1+pad; ++i) {
-    if (client.llen(i) > max_length) {
-      max_list = i;
-      max_length = client.llen(i);
-    }
+    multi.llen(i)
   }
-  res.send(max_list)
+  multi.exec(function (err, replies) {
+    var max_length = 0;
+    var max_list = 0;
+    for (var i = 0; i < min+1+pad; ++i) {
+      if (replies[i] > max_length) {
+        max_length = replies[i];
+        max_list = i;
+      }
+    }
+    res.send("" + max_list);
+  });
 });
 
 app.post('/put_request', function (req, res) {
   if (req.body.key && req.body.params) {
+    console.log(req.body.key);
+    console.log(req.body.params);
     for (var i = 0; i < min+1+pad; ++i) {
-      if (i != req.body.key) {
-        res.json(redis.rpush(i, req.body.res));
+      if ("" + i != req.body.key) {
+        client.rpush(i, req.body.params);
       }
     }
+    res.send("Yay!");
   }
+  else {res.send("Neh!");}
 });
 
 app.post('/get_request', function (req, res) {
   if (req.body.key) {
-    res.json(redis.blpop(req.body.key));
+    client.blpop(req.body.key, 0, function (err, reply) {
+      res.send(reply[1]);
+    });
   }
 });
 
-app.put('/put_answer', function (req, res) {
+app.post('/put_answer', function (req, res) {
   if (req.body.params && req.body.gain) {
-    redis.sadd(req.body.params, req.body.gain);
-    redis.expire(req.body.params, 300);
+    client.sadd(req.body.params, req.body.gain);
+    client.expire(req.body.params, 300);
+    res.send("Whoo!")
   }
+  res.send("Boohoo!");
 });
 
 app.post('/get_answers', function (req, res) {
   if (req.body.params) {
-    while (redis.scard(req.body.params) < min) {}
-    redis.expire(req.body.params, 300);
-    res.json(redis.smembers(req.body.params))
+    function loop(err, reply) {
+      client.expire(req.body.params, 300);
+      if (reply < min) {
+        client.scard(req.body.params, loop);
+      }
+      else {
+        client.smembers(req.body.params, function (err, reply) {
+          res.json(reply);
+        });
+      }
+    }
+    client.scard(req.body.params, loop);
   }
 });
+
+module.exports = app;
